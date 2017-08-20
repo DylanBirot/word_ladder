fc47bf977cdacd42abb7eaa57ac2f0e799ad194b
christova <30917374+christova@users.noreply.github.com>
2017-08-20 15:00:11 +1000

    Add files via upload
    The forbidden_build function and the same function have been implemented.

--- word_ladder_test.py
+++ word_ladder_test.py
@@ -2,13 +2,44 @@ import unittest
 import word_ladder
 
 
-class TestWordLadderFunctions(unittest.TestCase):
+class Test_Word_Ladder(unittest.TestCase):
+
 
     def test_file_load(self):
-        result = word_ladder.file_load()
-        return self.assertTrue(len(result) != 0)
+        pass
+    #test something here
+
+    def test_start_word_check(self):
+        pass
+    #self.assertEqual(word_ladder.start_word_check(), "Word cannot be a number \n\n\n")
+
+
+    def test_target_word_check(self):
+        pass
+    #self.assertEqual(word_ladder.start_word_check(), "Word cannot be a number \n\n\n")
 
 
 
-if __name__ == '__main_':
+    def test_path_selection(self):
+        pass
+    #test something here
+
+
+    def test_forbidden_build(self):
+        pass
+
+    # test something here
+
+
+
+    def test_same(self):
+        self.assertTrue(word_ladder.same("ate", "are") == 2)
+        self.assertTrue(word_ladder.same(1, 1) == TypeError)
+        self.assertEqual(word_ladder.same("eat", "ate"), 0)
+        self.assertEqual(word_ladder.same("test", "testing"), 4)
+        self.assertEqual(word_ladder.same("1", "1"), 1)
+        # self.assertRaises(TypeError, word_ladder.same(1,1))
+
+
+if __name__ == '__main__':
     unittest.main()
